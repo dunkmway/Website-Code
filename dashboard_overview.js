2cfd16da78c1631087e81c84f4d11be5caa1e71f
Duncan Morais <duncanmorais@Duncans-MacBook-Air-1000.local>
2020-11-11 20:53:39 -0700

    nps graph bug fixes

--- dashboard_overview.js
+++ dashboard_overview.js
@@ -183,6 +183,24 @@ firebase.auth().onAuthStateChanged(function(user) {
                                 }
 
                                 var ctx = document.getElementById('npsChart').getContext('2d');
+                                var chart = new Chart(ctx, {
+                                    // The type of chart we want to create
+                                    type: 'line',
+                                
+                                    // The data for our dataset
+                                    data: {
+                                        labels: npsDateArray,
+                                        datasets: [{
+                                            label: 'My First dataset',
+                                            backgroundColor: '#707070',
+                                            borderColor: '#707070',
+                                            data: npsTotalScores
+                                        }]
+                                    },
+                                
+                                    // Configuration options go here
+                                    options: {}
+                                });
                                 
                             }
                             else {
