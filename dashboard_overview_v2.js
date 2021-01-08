ef069175b70b189e1a53e7ac39fb215de7d296b8
Duncan Morais <duncanmorais@gmail.com>
2021-01-07 23:42:34 -0700

    bug fixes

--- dashboard_overview_v2.js
+++ dashboard_overview_v2.js
@@ -164,7 +164,7 @@ function calculateNpsScore(dateIndex, trailing, count, detractors, promoters) {
     console.log(count.length);
     for (var i = 0; i < trailing; i++) {
         totalCount += count[dateIndex + i];
-        console.log(count[i]);
+        console.log(count[0]);
         totalDetractors += detractors[dateIndex + i];
         totalPromoters += promoters[dateIndex + i];
     }
